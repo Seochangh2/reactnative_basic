406ebeb1a4f483040ba3b137f805c40d2b71e952
Seochangh2 <nomm89@naver.com>
2022-03-21 22:02:27 +0900

    flex

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Vibration } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        텍스트는 반드시 텍스트 안에 , div 대신 view , 자동완성을 원할경우
-        StyleSheet
-      </Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 3 }}>
+        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+        <View style={{ flex: 2, backgroundColor: "blue" }}></View>
+        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+      </View>
+      <View style={{ flex: 2 }}>
+        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+        <View style={{ flex: 2, backgroundColor: "blue" }}></View>
+        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+      </View>
     </View>
   );
 }
