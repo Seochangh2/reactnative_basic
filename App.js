d6a188289bcd2f0520989359d0164f1eeb1945c7
Seochangh2 <nomm89@naver.com>
2022-03-21 21:18:34 +0900

    리액트 네이티브

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        텍스트는 반드시 텍스트 안에 , div 대신 view , 자동완성을 원할경우
+        StyleSheet
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "blue",
   },
 });
