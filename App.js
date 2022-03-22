b85326570b2e1d877721eb20879ae8fee90b54ab
Seochangh2 <nomm89@naver.com>
2022-03-23 01:54:12 +0900

    어플의 기본 ui

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View, Vibration } from "react-native";
+import {
+  Dimensions,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 3 }}>
-        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-        <View style={{ flex: 2, backgroundColor: "blue" }}></View>
-        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+    <View style={styles.container}>
+      <StatusBar style="black" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={{ flex: 2 }}>
-        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-        <View style={{ flex: 2, backgroundColor: "blue" }}></View>
-        <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
-      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator="false"
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -21,12 +51,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "plum",
   },
-  text: {
-    fontSize: 30,
-    color: "blue",
+  city: {
+    flex: 1,
+    justifyContent: "center", // 세로
+    alignItems: "center", //가로
+  },
+  cityName: {
+    fontSize: 70,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center", //가로
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 158,
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 100,
   },
 });
